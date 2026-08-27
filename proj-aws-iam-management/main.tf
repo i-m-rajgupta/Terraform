3332757d2edbf24ae32623173def692df2fc5044
Raj <neeleshgupta32154@gmail.com>
2026-08-28 00:21:25 +0530

    Added Iam task

---
+++ proj-aws-iam-management/main.tf
@@ -0,0 +1,65 @@
+terraform {
+  required_providers {
+    aws ={
+        source = "hashicorp/aws"
+        version = "6.58.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+locals {
+  users_data = yamldecode(file("./users.yaml")).users
+  user_role_pair = flatten([for user in local.users_data: [for role in user.roles: {
+    username = user.username
+    role = role
+  }]])
+}
+
+output "user" {
+  value = local.user_role_pair
+}
+
+#Creatung user
+resource "aws_iam_user" "main" {
+  for_each = toset(local.users_data[*].username)
+  name = each.value
+}
+
+resource "aws_iam_user_login_profile" "profile" {
+  for_each = aws_iam_user.main
+  user = each.value.name
+  password_length = 12
+
+  lifecycle {
+    ignore_changes = [ 
+        password_length,
+        password_reset_required,
+        pgp_key
+     ]
+  }
+}
+
+# #Attaching policies
+resource "aws_iam_user_policy_attachment" "main" {
+  for_each = {
+    for pair in local.user_role_pair :
+    "${pair.username}-${pair.role}" => pair 
+  }
+  user = aws_iam_user.main[each.value.username].name
+  policy_arn = "arn:aws:iam::aws:policy/${each.value.role}"
+}
+
+# locals {
+#   user = keys( {
+#     for pair in local.user_role_pair :
+#     "${pair.username}-${pair.role}" => pair 
+#   })
+# }
+
+# output "users_policy" {
+#   value = local.user
+# }
