2446111c652f2289da9551c31aabe22761ddbfa5
Raj <neeleshgupta32154@gmail.com>
2026-09-10 16:27:11 +0530

    Added cli workspace file

---
+++ tf-cli-workspace/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "6.58.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "random_id" "rand_id" {
+  byte_length = 8
+}
+
+resource "aws_s3_bucket" "demo-bucket" {
+  bucket = "demo-bucket-${terraform.workspace}-${random_id.rand_id.hex}"
+}
